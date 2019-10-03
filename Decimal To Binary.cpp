6100674806c0cd72b50c73a7a091e6bf078d2c3d
Prateek Kr. Gupta <30932967+prateek45@users.noreply.github.com>
2019-10-03 20:10:21 +0530

    file changes
    made some changes in indentation and applied shortcuts use in c++

--- Decimal To Binary.cpp
+++ Decimal To Binary.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main()
 {
-	int number;
-	cin>>number;
-	int remainder,binary=0,var=1;
+      int number;
+      cin>>number;
+      int remainder,binary=0,var=1;
 
-do{
+    do{
 		
 	 remainder=number%2;
-	number=number/2;
-	binary=binary+(remainder*var);
-	var=var*10;
+	 number=number/2;
+	 binary+=(remainder*var);
+	 var=var*10;
 
 
-}
-while(number>0);
-	cout<<binary;
+      }while(number>0);
+	
+     cout<<binary;
 }
